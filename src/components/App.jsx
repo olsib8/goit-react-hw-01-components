0df570191e0422cef8812e3fb08a376ee322fd13
Oleksandr Sibirtsev <alsib8@gmail.com>
2023-04-18 22:06:57 +0300

    task-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import { Profile } from './profile/Profile';
 import user from './profile/user.json';
 import { Statistics } from './statistics/Statistics';
 import data from './statistics/data.json';
+import { FriendList } from './friendList/FriendList';
+import friends from './friendList/friends.json';
 
 export const App = () => {
   return (
@@ -23,6 +25,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
